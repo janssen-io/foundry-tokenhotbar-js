ab41ff283720c5fe1978e138c9dd00fb3a203b6f
Stan Janssen <mail@janssen.io>
2021-11-06 22:08:55 +0100

    Implemented User Hotbar
    - Save hotbar when no token is selected under user id.
    - Reload this hotbar when a token was controlled, but then no longer is.
    - TODO: save initial hotbar when module is loaded for the first time

--- src/features.mjs
+++ src/features.mjs
@@ -3,19 +3,14 @@ import { debug, TH } from '../lib/constants.mjs'
 /**
  *
  * @param {{ id: string}[]} controlledTokens
- * @param {{id: string, setFlag: function}} currentUser
- * @param {{id: string, setFlag: function}} user
+ * @param {{id: string, unsetFlag: function, setFlag: function}} currentUser
+ * @param {{id: string, unsetFlag: function, setFlag: function}} user
  * @param {{hotbar: {}}} data
  * @returns the saved Token Hotbar object
  */
-export function saveHotbar(controlledTokens, currentUser, user, data) {
+export async function saveHotbar(controlledTokens, currentUser, user, data) {
     if (!data.hotbar) {
         debug("User updated, but no new hotbar data present.", data)
-        return undefined;
-    }
-
-    if (controlledTokens.length !== 1) {
-        debug("Not updating any hotbar, not exactly one token selected.", controlledTokens);
         return undefined;
     }
 
@@ -24,36 +19,64 @@ export function saveHotbar(controlledTokens, currentUser, user, data) {
         return undefined;
     }
 
-    // For now, we write our logic here. Perhaps later we want to separate it for readability/maintainability.
-    // Concept: ui-hotbar refers to the data from the hotbar on the bottom of the screen.
-    //          updates contain the complete current page in the format { number: { slot: number, macro: Macro } }
-    const updatedUiHotbar = data.hotbar;
-    const token = controlledTokens[0];
+    if (controlledTokens.length === 0) {
+        // Code copied from saving hotbar for token (controlled.length = 1)
+        const updatedUiHotbar = data.hotbar;
 
-    // let's implement storing the hotbar on the user first. This is the simplest, as we don't need to update other clients.
-    const documentToStoreTokenHotbar = user;
-    const tokenHotbar = updatedUiHotbar;
+        const documentToStoreTokenHotbar = user;
+        const tokenHotbar = updatedUiHotbar;
 
-    documentToStoreTokenHotbar.setFlag(TH.name, `hotbar.${token.id}`, updatedUiHotbar);
-    return tokenHotbar;
+        debug("Storing hotbar for user", tokenHotbar);
+        // use the user id as we are storing the hotbar of the user
+        await documentToStoreTokenHotbar.unsetFlag(TH.name, `hotbar.${user.id}`);
+        await documentToStoreTokenHotbar.setFlag(TH.name, `hotbar.${user.id}`, updatedUiHotbar);
+        return tokenHotbar;
+    } else if(controlledTokens.length === 1) {
+        // For now, we write our logic here. Perhaps later we want to separate it for readability/maintainability.
+        // Concept: ui-hotbar refers to the data from the hotbar on the bottom of the screen.
+        //          updates contain the complete current page in the format { number: { slot: number, macro: Macro } }
+        const updatedUiHotbar = data.hotbar;
+        const token = controlledTokens[0];
+
+        // let's implement storing the hotbar on the user first. This is the simplest, as we don't need to update other clients.
+        const documentToStoreTokenHotbar = user;
+        const tokenHotbar = updatedUiHotbar;
+
+        debug("Storing hotbar for token", token, tokenHotbar);
+        // use the token id as we are storing the hotbar of the token
+        await documentToStoreTokenHotbar.unsetFlag(TH.name, `hotbar.${token.id}`);
+        await documentToStoreTokenHotbar.setFlag(TH.name, `hotbar.${token.id}`, updatedUiHotbar);
+        return tokenHotbar;
+    } else {
+        debug("Not updating any hotbar, not exactly one token selected.", controlledTokens);
+        return;
+    }
+
 }
 
 /**
  *
- * @param user
- * @param controlledTokens
+ * @param {{ id: string}[]} controlledTokens
+ * @param {{id: string, getFlag: function, data: { update: function }}} user
  * @returns True, if hotbar has been loaded.
  */
 export function loadHotbar(user, controlledTokens) {
-    if (controlledTokens.length !== 1) {
-        debug("Not loading any hotbar, not exactly one token selected.", controlledTokens);
+    const documentWithTokenHotbar = user;
+    if (controlledTokens.length === 0) {
+        const hotbarForToken = documentWithTokenHotbar.getFlag(TH.name, `hotbar.${user.id}`);
+        debug("Loading Hotbar for user", hotbarForToken);
+        // Use { recursive: false } to replace the hotbar, instead of merging it.
+        user.data.update({ hotbar: hotbarForToken || {} }, { recursive: false });
+        return true;
+    } else if (controlledTokens.length === 1) {
+        const token = controlledTokens[0];
+        const hotbarForToken = documentWithTokenHotbar.getFlag(TH.name, `hotbar.${token.id}`);
+        debug("Loading Hotbar for token", token, hotbarForToken);
+        // Use { recursive: false } to replace the hotbar, instead of merging it.
+        user.data.update({ hotbar: hotbarForToken || {} }, { recursive: false });
+        return true;
+    } else {
+        debug("Not loading any hotbar, more than one token selected.", controlledTokens);
         return false;
     }
-
-    const token = controlledTokens[0];
-    const hotbarForToken = user.getFlag(TH.name, `hotbar.${token.id}`);
-    debug("Loading Hotbar", token, hotbarForToken);
-    // Use { recursive: false } to replace the hotbar, instead of merging it.
-    user.data.update({ hotbar: hotbarForToken }, { recursive: false });
-    return true;
 }
