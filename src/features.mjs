832594e0e1ac2142f9242a156e325dc6445bcf60
Stan Janssen <mail@janssen.io>
2021-11-07 17:23:42 +0100

    Clean up the code
    Moved the logic to determine which entity to store the hotbar for
    to a separate function that can be used for both loading and storing the
    hotbar.
    
    Reduce duplication of saving/loading the hotbar by moving it out of the
    branches.

--- src/features.mjs
+++ src/features.mjs
@@ -3,7 +3,7 @@ import { settingKeys } from './settings.mjs'
 
 /**
  *
- * @param {{ id: string}[]} controlledTokens
+ * @param {{ id: string, actor: { id: string }, document: object }[]} controlledTokens
  * @param {{id: string, unsetFlag: function, setFlag: function}} currentUser
  * @param {{id: string, unsetFlag: function, setFlag: function}} user
  * @param {{hotbar: {}}} data
@@ -21,74 +21,67 @@ export async function saveHotbar(controlledTokens, currentUser, user, data, getS
         return;
     }
 
-    if (controlledTokens.length === 0) {
-        // Code copied from saving hotbar for token (controlled.length = 1)
-        const updatedUiHotbar = data.hotbar;
-
-        const documentToStoreTokenHotbar = user;
-        const tokenHotbar = updatedUiHotbar;
-
-        debug("Storing hotbar for user", tokenHotbar);
-        // use the user id as we are storing the hotbar of the user
-        await documentToStoreTokenHotbar.unsetFlag(TH.name, `hotbar.${user.id}`);
-        await documentToStoreTokenHotbar.setFlag(TH.name, `hotbar.${user.id}`, updatedUiHotbar);
-        return tokenHotbar;
-    } else if(controlledTokens.length === 1) {
-        // For now, we write our logic here. Perhaps later we want to separate it for readability/maintainability.
-        // Concept: ui-hotbar refers to the data from the hotbar on the bottom of the screen.
-        //          updates contain the complete current page in the format { number: { slot: number, macro: Macro } }
-        const updatedUiHotbar = data.hotbar;
-        const token = controlledTokens[0] ;
-        // If the token is linked to an actor (e.g. PC) or we always want to link hotbars to (synthetic) actors
-        // Then use the actor to store the hotbar
-        // Otherwise use the token
-        const entityToCreateHotbarFor =
-            getSetting(settingKeys.alwaysUseActor) || token.document.isLinked
-            ? token.actor : token;
-
-        // let's implement storing the hotbar on the user first. This is the simplest, as we don't need to update other clients.
-        const documentToStoreTokenHotbar = user;
-        const tokenHotbar = updatedUiHotbar;
-
-        debug("Storing hotbar for token", entityToCreateHotbarFor, tokenHotbar);
-        // use the token id as we are storing the hotbar of the token
-        await documentToStoreTokenHotbar.unsetFlag(TH.name, `hotbar.${entityToCreateHotbarFor.id}`);
-        await documentToStoreTokenHotbar.setFlag(TH.name, `hotbar.${entityToCreateHotbarFor.id}`, updatedUiHotbar);
-        return tokenHotbar;
-    } else {
-        debug("Not updating any hotbar, not exactly one token selected.", controlledTokens);
+    if (controlledTokens.length > 1) {
+        debug("Not updating any hotbar, more than one token selected.", controlledTokens);
         return;
     }
 
+    const entity = determineEntityForHotbar(controlledTokens, user, getSetting);
+    const documentWithHotbar = user;
+    await storeHotbar(data.hotbar, documentWithHotbar, entity);
+    return data.hotbar;
 }
 
 /**
  *
- * @param {{ id: string}[]} controlledTokens
+ * @param {{ id: string, actor: { id: string }, document: object }[]} controlledTokens
  * @param {{id: string, getFlag: function, data: { update: function }}} user
  * @param {function} getSetting
  * @returns True, if hotbar has been loaded.
  */
 export function loadHotbar(user, controlledTokens, getSetting) {
-    const documentWithTokenHotbar = user;
-    if (controlledTokens.length === 0) {
-        const hotbarForToken = documentWithTokenHotbar.getFlag(TH.name, `hotbar.${user.id}`);
-        debug("Loading Hotbar for user", hotbarForToken);
-        // Use { recursive: false } to replace the hotbar, instead of merging it.
-        user.data.update({ hotbar: hotbarForToken || {} }, { recursive: false });
-        return true;
-    } else if (controlledTokens.length === 1) {
-        const token = controlledTokens[0];
-        const entityToGetHotbarFor =
-            getSetting(settingKeys.alwaysUseActor) || token.document.isLinked
-            ? token.actor : token;
-        const hotbarForToken = documentWithTokenHotbar.getFlag(TH.name, `hotbar.${entityToGetHotbarFor.id}`);
-        debug("Loading Hotbar for token", entityToGetHotbarFor, hotbarForToken);
-        // Use { recursive: false } to replace the hotbar, instead of merging it.
-        user.data.update({ hotbar: hotbarForToken || {} }, { recursive: false });
-        return true;
-    } else {
+    if (controlledTokens.length > 1) {
         debug("Not loading any hotbar, more than one token selected.", controlledTokens);
         return false;
     }
+
+    const documentWithHotbar = user;
+    const entity = determineEntityForHotbar(controlledTokens, user, getSetting);
+    const hotbarForToken = documentWithHotbar.getFlag(TH.name, `hotbar.${entity.id}`);
+
+    debug(`Loading Hotbar for ${entity.constructor.name} from document`, { documentWithHotbar, entity, hotbarForToken });
+
+    // Use { recursive: false } to replace the hotbar, instead of merging it.
+    user.data.update({ hotbar: hotbarForToken || {} }, { recursive: false });
+    return true;
+}
+
+function determineEntityForHotbar(controlledTokens, user, getSetting) {
+    if (controlledTokens.length === 0) {
+        // use the user id as we are storing the hotbar of the user
+        return user;
+    } else if (controlledTokens.length === 1) {
+        const token = controlledTokens[0];
+        // If the token is linked to an actor (e.g. PC)
+        //    OR we always want to link hotbars to (synthetic) actors
+        // Then use the actor to store the hotbar
+        // Otherwise use the token
+        return getSetting(settingKeys.alwaysUseActor) || token.document.isLinked
+            ? token.actor
+            : token;
+    }
+}
+
+/**
+ * Store the entity's hotbar onto some document.
+ * The entity's ID will be used to store different hotbars on the same document.
+ * @param hotbarToStore The hotbar to store
+ * @param documentWithHotbar The document to store the hotbar onto (in their flags)
+ * @param entity The entity to store the hotbar for. Usually the user itself, the token or its actor.
+ */
+async function storeHotbar(hotbarToStore, documentWithHotbar, entity) {
+    debug(`Storing hotbar for ${entity.constructor.name} on document`, { documentWithHotbar, entity, hotbarToStore });
+    // use the token id as we are storing the hotbar of the token
+    await documentWithHotbar.unsetFlag(TH.name, `hotbar.${entity.id}`);
+    await documentWithHotbar.setFlag(TH.name, `hotbar.${entity.id}`, hotbarToStore);
 }
