223fe1757520d084f6f174b599ed6bbd63aa773f
Stan Janssen <mail@janssen.io>
2021-11-07 16:51:46 +0100

    Add timeout to prevent race condition when controlling another token

--- index.mjs
+++ index.mjs
@@ -34,13 +34,24 @@ Hooks.on("updateUser", (user, data) => {
 
 // We use the controlToken [controlPlaceableObject] hook
 //  - It's fired when a token is controlled (or let go)
+
+// We need to use a timeout, because going from one controlled token to another
+// fires two hooks before we can complete our logic. We only care about the last one.
+
+let controlTokenTimeout;
 Hooks.on("controlToken", (object, isControlled) => {
-    const controlledTokens = game.canvas.tokens.controlled;
-    const getSetting = getModuleSettings(game.settings);
-    if(loadHotbar(game.user, controlledTokens, getSetting)) {
-        // Only re-render the hotbar, if it actually changed because of us
-        ui.hotbar.render();
+    if (controlTokenTimeout) {
+        window.clearTimeout(controlTokenTimeout);
     }
+
+    controlTokenTimeout = window.setTimeout(() => {
+        const controlledTokens = game.canvas.tokens.controlled;
+        const getSetting = getModuleSettings(game.settings);
+        if(loadHotbar(game.user, controlledTokens, getSetting)) {
+            // Only re-render the hotbar, if it actually changed because of us
+            ui.hotbar.render();
+        }
+    }, 100);
 });
 
 // Note: we try to stay clear from global variables (game.canvas for example)
